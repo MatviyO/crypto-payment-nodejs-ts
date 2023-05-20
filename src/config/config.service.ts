e3bd4fb200dbea47eba16ad913612065e365188a
Oleh <oleh.m97@gmail.com>
2023-05-20 15:18:40 +0300

    init

---
+++ src/config/config.service.ts
@@ -0,0 +1,18 @@
+import { DotenvParseOutput, config } from 'dotenv';
+import { IConfigService } from './config.interface';
+
+export class ConfigService implements IConfigService {
+  private readonly config: DotenvParseOutput;
+
+  constructor() {
+    const { error, parsed } = config();
+    if (error || !parsed) throw new Error('Config doest found');
+    this.config = parsed;
+  }
+
+  get(key: string): string {
+    const res = this.config[key];
+    if (!res) throw new Error('Config doest found');
+    return res;
+  }
+}
